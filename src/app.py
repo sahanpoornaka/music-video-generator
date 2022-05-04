cb0839f387a031219950887cf0e21b59566fd1ca
SahanAtNeura <sahan@neuraintelligence.com>
2022-05-05 02:32:25 +0530

    Added Upto Scene Detection and Sampling

--- src/app.py
+++ src/app.py
@@ -8,10 +8,14 @@ from modules import scene_detect, clip_video, diagrams
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 sys.path.append(ROOT_DIR)
 
-INPUT_FILE = "/home/sahan/Desktop/Projects/DataDisca/Music_Video_Generator/resources/music_video/mal_kakuli.mp4"
-FORMATTED_DIR = "/home/sahan/Desktop/Projects/DataDisca/Music_Video_Generator/resources/formatted_files"
+INPUT_FILE = "/home/sahan/Desktop/Projects/DataDisca/Music_Video_Generator/src/resources/mal_kakuli.mp4"
+FORMATTED_DIR = "/home/sahan/Desktop/Projects/DataDisca/Music_Video_Generator/src/resources/formatted_files"
+OUTPUT_FILE_NAME = "1"
 
-# scenes = scene_detect.find_scenes(INPUT_FILE)
-# clip_video.clip_video(INPUT_FILE, scenes, FORMATTED_DIR+"/"+str(1))
+scenes = scene_detect.find_scenes(INPUT_FILE)
+clip_video.clip_video_from_list(INPUT_FILE, scenes, FORMATTED_DIR+"/"+OUTPUT_FILE_NAME)
 
-diagrams.generate_diagrams(FORMATTED_DIR)
+
+# diagrams.generate_diagrams(FORMATTED_DIR)
+
+print("Done")
